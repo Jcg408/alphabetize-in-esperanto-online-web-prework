ce0a1a3f473a859514238d95f832de4677debe65
Jessica Gladstone <jcg321@comcast.net>
2018-10-21 19:23:53 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-    esp_arr = ALPHABET.split ("")
+    esp_arr = ALPHABET.split ("") #Ruby constant must be caps. Make sure no space in parameters
     arr.sort_by! do |section|
         (section.split("").collect {|letter|esp_arr.index(letter)}).compact
     end
