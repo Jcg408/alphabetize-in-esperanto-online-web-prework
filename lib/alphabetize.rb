005b7b4e745e9b0212184854c01852b74d9267d5
Jessica Gladstone <jcg321@comcast.net>
2018-10-21 19:24:06 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-    esp_arr = ALPHABET.split ("") #Ruby constant must be caps. Make sure no space in parameters
+    esp_arr = ALPHABET.split ("") #Ruby constant must be caps. Make sure no space in quotes
     arr.sort_by! do |section|
         (section.split("").collect {|letter|esp_arr.index(letter)}).compact
     end
