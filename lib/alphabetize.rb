198f2a7cc0c39ad6ba21a7ed7d8e0dc532974742
Jessica Gladstone <jcg321@comcast.net>
2018-10-21 19:19:05 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
     esp_arr = ALPHABET.split ("")
     arr.sort_by! do |section|
-        (section.split(" ").collect {|letter| esp_arr.index(letter)}).compact
+        (section.split("").collect {|letter| esp_arr.index(letter)}).compact
     end
 end
